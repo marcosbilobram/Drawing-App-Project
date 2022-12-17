75bde597d45a37333cdfa45a285303a8a39db02f
marcosbilobram <marcosbilobram@hotmail.com>
2022-12-17 12:50:09 -0300

    Starting project, setting up drawing view

--- app/src/main/java/com/project/mydrawingapp/DrawingView.kt
+++ app/src/main/java/com/project/mydrawingapp/DrawingView.kt
@@ -1,7 +1,42 @@
 package com.project.mydrawingapp
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+
+class DrawingView(context : Context, attrs : AttributeSet) : View(context, attrs) {
+
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitMap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
+
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat()
+    }
 
 
-class DrawingView{
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
+
+    }
 
 }
